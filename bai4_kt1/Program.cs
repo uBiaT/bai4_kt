5b7e64635830c4d7428f79183e097e0529822b40
hv <hv@PM309-02>
2023-03-26 09:58:53 +0700

    Add project files.

---
+++ bai4_kt1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kiemtra1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("<----------------------INPUT---------------------->");
+            Console.Write("Nhap mot so nguyen duong: ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            int tong = 0;
+            int tich = 1;
+
+            for (int i = 1; i <= n; i++)
+            {
+                tich *= i;
+                tong += tich;
+            }
+
+            Console.WriteLine("<----------------------OUTPUT--------------------->");
+            Console.WriteLine($"Tong giai thua cac so tu 1 toi {n} la {tong}");
+            Console.WriteLine();
+
+            Console.WriteLine("<----------------------END------------------------>");
+            Console.WriteLine("Nhan phim bat ki de ket thuc");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ bai4_kt2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kiemtra2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo cki;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("<----------------------INPUT---------------------->");
+                Console.Write("Nhap mot so nguyen duong: ");
+                int n = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+
+                Boolean laSoNguyenTo;
+                int tong = 0;
+
+                Console.WriteLine("<----------------------OUTPUT--------------------->");
+                for (int i = 2; i <= n; i++)
+                {
+                    laSoNguyenTo = true;
+                    for (int j = 2; j < i; j++)
+                    {
+                        if (i % j == 0)
+                        {
+                            laSoNguyenTo = false;
+                            break;
+                        }
+                    }
+                    if (laSoNguyenTo == true)
+                    {
+                        Console.WriteLine(i);
+                        tong += i;
+                    }
+                }
+
+                Console.WriteLine($"Tong cac so nguyen to tu 1 toi {n} la {tong}");
+                Console.WriteLine();
+
+                Console.WriteLine("<----------------------END------------------------>");
+                Console.WriteLine("Nhan phim phim BAT KY de tiep tuc hoac phim ESC de ket thuc");
+                Console.WriteLine();
+                cki = Console.ReadKey();
+            } while ((cki.Key != ConsoleKey.Escape));
+        }
+    }
+}
